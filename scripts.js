2a54fb1aaf7873f30575a6047cb92f340b2914b5
Armando Galván <armandocgv@gmail.com>
2017-01-07 00:28:07 -0600

    More examples of traversing arrays

--- scripts.js
+++ scripts.js
@@ -89,12 +89,23 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const isAdult = people2.some(person => (new Date()).getFullYear() - person.year >= 19);
+console.log(isAdult)
 // Array.prototype.every() // is everyone 19 or older?
+const allAdult = people2.every(person => (new Date()).getFullYear() - person.year >= 19);
+console.log(allAdult);
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const comment = comments.find(com => com.id === 823423);
 
+console.log(comment);
 // Array.prototype.findIndex()
 // Find the comment with this ID
-// delete the comment with the ID of 823423
+const index = comments.findIndex(com => com.id === 823423);
+console.log(index);
+
+// delete the comment with the ID of 
+comments.splice(index, 1);
+console.table(comments);
