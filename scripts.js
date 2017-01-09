5a851f0e2664227953455e5e10dbb512402bcd26
Armando Galván <armandocgv@gmail.com>
2017-01-08 21:33:08 -0600

    All console data displaying correctly

--- scripts.js
+++ scripts.js
@@ -23,27 +23,28 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 const fifteen = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
-//console.table(fifteen);
+console.table(fifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
 const fullNames = inventors.map(inventor => inventor.first + ' ' +inventor.last);
-//console.log(fullNames);
+console.log(fullNames);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 const ordered = inventors.sort((a,b) => a.year > b.year ? 1:-1);
-//console.table(ordered);
+console.table(ordered);
+
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
 const totalYears = inventors.reduce((total, inventor) => {
   return total + (inventor.passed - inventor.year);
 }, 0);
-//console.log(totalYears);
+console.log(totalYears);
 
 // 5. Sort the inventors by years lived
 const yearsLived = inventors.sort((a,b) => (a.passed - a.year > b.passed - b.year) ? 1:-1);
-//console.table(yearsLived);
+console.table(yearsLived);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
@@ -51,6 +52,7 @@ const yearsLived = inventors.sort((a,b) => (a.passed - a.year > b.passed - b.yea
 const links = [...category.querySelectorAll('a')];
 const de = links.map(link => link.textContent).filter(streetName => streetName.includes('de'));
 console.log(de);*/
+
 // 7. sort Exercise
 // Sort the people alphabetically by last name
 const alpha = people.sort((lastOne, nextOne) => {
@@ -58,7 +60,7 @@ const alpha = people.sort((lastOne, nextOne) => {
     const [bLast, bFirst] = nextOne.split(',');
     return aLast > bLast ? 1:-1;
 });
-//console.log(alpha);
+console.log(alpha);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
@@ -68,7 +70,7 @@ const transportation = data.reduce((obj, item) => {
   obj[item]++;
   return obj;
 }, {});
-//console.log(transportation);
+console.log(transportation);
 
 // ## Array Cardio Day 2
 
